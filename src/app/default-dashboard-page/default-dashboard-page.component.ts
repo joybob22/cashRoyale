a5026a3dbd06a04f94c5c118733097745c2df148
Andrew <ward.spacecase@gmail.com>
2017-04-21 14:09:30 -0600

    chart nav

--- src/app/default-dashboard-page/default-dashboard-page.component.ts
+++ src/app/default-dashboard-page/default-dashboard-page.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-default-dashboard-page',
   templateUrl: './default-dashboard-page.component.html',
-  styleUrls: ['./default-dashboard-page.component.scss']
+  styleUrls: ['./default-dashboard-page.component.css']
 })
 export class DefaultDashboardPageComponent implements OnInit {
 
