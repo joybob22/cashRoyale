422ef94130dcfa3f7344f920d921d0d2ed73e99f
joybob22 <braydenlemke8@gmail.com>
2017-04-21 12:59:05 -0600

    stuff

--- src/app/default-dashboard-page/default-dashboard-page.component.ts
+++ src/app/default-dashboard-page/default-dashboard-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-default-dashboard-page',
   templateUrl: './default-dashboard-page.component.html',
-  styleUrls: ['./default-dashboard-page.css']
+  styleUrls: ['./default-dashboard-page.component.scss']
 })
 export class DefaultDashboardPageComponent implements OnInit {
 
