e5353685cf397a1ee9ad3626a8c911b75651f35a
joybob22 <braydenlemke8@gmail.com>
2017-04-21 08:32:58 -0600

    made initial pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { BudgetPageComponent } from './budget-page/budget-page.component';
+import { CheckbookPageComponent } from './checkbook-page/checkbook-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+    DashboardPageComponent,
+    BudgetPageComponent,
+    CheckbookPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/checkbook-page/checkbook-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-checkbook-page',
+  templateUrl: './checkbook-page.component.html',
+  styleUrls: ['./checkbook-page.component.css']
+})
+export class CheckbookPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
