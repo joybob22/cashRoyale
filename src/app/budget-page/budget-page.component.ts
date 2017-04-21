85c7c46747de5812ca86b3cb98c42ac235f3b6f3
ThargelionZ <measles10@gmail.com>
2017-04-21 17:46:59 -0600

    Well started budget page

--- src/app/budget-page/budget-page.component.ts
+++ src/app/budget-page/budget-page.component.ts
@@ -6,8 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./budget-page.component.scss']
 })
 export class BudgetPageComponent implements OnInit {
-
-  constructor() { }
+  budget = [{categoryName: 'groceries',
+             items: [{category: 'groceries', purchase: 'milk', quantity: 1, price: '$2.75', notes: 'Need 4 more next week'},
+                     {category: 'groceries', purchase: 'fruit', quantity: 4, price: '$1.75', notes: 'Need 2 more next week'},
+                     {category: 'groceries', purchase: 'bread', quantity: 2, price: '$2.75', notes: 'Need 4 more next week'}],
+             total: '$45.78'
+            },
+            {categoryName: 'auto',
+              items: [{category: 'auto', purchase: 'oil change', quantity: 1, price: '$10.75', notes: 'Need one more next month'}],
+              total: '$45.78'
+            }
+            ];
+  constructor() {}
 
   ngOnInit() {
   }
