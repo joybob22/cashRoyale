ccfab5b1999f97014384c7dd6741e73cc9ade754
joybob22 <braydenlemke8@gmail.com>
2017-04-21 08:48:00 -0600

    added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { RegisterPageComponent } from './register-page/register-page.component';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { BudgetPageComponent } from './budget-page/budget-page.component';
 import { CheckbookPageComponent } from './checkbook-page/checkbook-page.component';
+import { RouterModule } from "@angular/router";
+import { DefaultDashboardPageComponent } from './default-dashboard-page/default-dashboard-page.component';
 
 @NgModule({
   declarations: [
@@ -19,12 +21,28 @@ import { CheckbookPageComponent } from './checkbook-page/checkbook-page.componen
     RegisterPageComponent,
     DashboardPageComponent,
     BudgetPageComponent,
-    CheckbookPageComponent
+    CheckbookPageComponent,
+    DefaultDashboardPageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'homePage', component: HomePageComponent},
+      { path: 'loginPage', component: LoginPageComponent},
+      { path: 'registerPage', component: RegisterPageComponent},
+      { path: 'dashboardPage', component: DashboardPageComponent,
+        children: [
+          { path: 'defaultDashboardPage', component: DefaultDashboardPageComponent},
+          { path: 'checkbookPage', component: CheckbookPageComponent},
+          { path: 'budgetPage', component: BudgetPageComponent},
+          { path: '', redirectTo: 'defaultDashboardPage', pathMatch: 'full'}
+        ]
+      },
+      { path: '', redirectTo: 'homePage', pathMatch: 'full'}
+      // { path: '**', component: PageNotFoundComponent}
+    ], {useHash: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/default-dashboard-page/default-dashboard-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-default-dashboard-page',
+  templateUrl: './default-dashboard-page.component.html',
+  styleUrls: ['./default-dashboard-page.component.css']
+})
+export class DefaultDashboardPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
