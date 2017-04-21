b92404154be1fb507dea740329a9db4e1c352065
ThargelionZ <measles10@gmail.com>
2017-04-21 11:38:59 -0600

    Linked all scss files

--- src/app/checkbook-page/checkbook-page.component.ts
+++ src/app/checkbook-page/checkbook-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-checkbook-page',
   templateUrl: './checkbook-page.component.html',
-  styleUrls: ['./checkbook-page.component.css']
+  styleUrls: ['./checkbook-page.component.scss']
 })
 export class CheckbookPageComponent implements OnInit {
 
--- src/app/register-page/register-page.component.ts
+++ src/app/register-page/register-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
-  styleUrls: ['./register-page.component.css']
+  styleUrls: ['./register-page.component.scss']
 })
 export class RegisterPageComponent implements OnInit {
 
