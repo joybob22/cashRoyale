068404da510b2d877944f2cd7fc243477e0401fa
ThargelionZ <measles10@gmail.com>
2017-04-21 12:47:43 -0600

    Added correct scss files and links

--- src/app/checkbook-page/checkbook-page.component.ts
+++ src/app/checkbook-page/checkbook-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-checkbook-page',
   templateUrl: './checkbook-page.component.html',
-  styleUrls: ['./checkbook-page.scss']
+  styleUrls: ['./checkbook-page.component.scss']
 })
 export class CheckbookPageComponent implements OnInit {
 
