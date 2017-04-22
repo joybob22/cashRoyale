1a3d49925f176ad806f426ffe6760374b5bc1e02
ThargelionZ <measles10@gmail.com>
2017-04-22 04:27:17 -0600

    Updated the lastest budget page

--- src/app/budget-page/budget-page.component.ts
+++ src/app/budget-page/budget-page.component.ts
@@ -10,22 +10,23 @@ export class BudgetPageComponent implements OnInit {
 
   inputPurchaseValue: string;
   inputQuantityValue: number;
-  inputPriceValue: string;
+  inputPriceValue: any;
   inputNotesValue: string;
 
+  total: any;
+  categoryTotal: any;
+
   budget = [{categoryName: 'groceries',
-             items: [{category: 'groceries', purchase: 'milk', quantity: 1, price: '$2.75', notes: 'Need 4 more next week'},
-                     {category: 'groceries', purchase: 'fruit', quantity: 4, price: '$1.75', notes: 'Need 2 more next week'},
-                     {category: 'groceries', purchase: 'bread', quantity: 2, price: '$2.75', notes: 'Need 4 more next week'}],
-             total: '$45.78'
+             items: [{category: 'groceries', purchase: 'milk', quantity: 1, price: '$2.75', total: '$2.75', notes: 'Need 4 more next week'},
+                     {category: 'groceries', purchase: 'fruit', quantity: 4, price: '$1.75', total: '$7.00', notes: 'Need 2 more next week'},
+                     {category: 'groceries', purchase: 'bread', quantity: 2, price: '$2.75', total: '$4.50', notes: 'Need 4 more next week'}],
+             total: this.categoryTotal
             },
             {categoryName: 'auto',
-              items: [{category: 'auto', purchase: 'oil change', quantity: 1, price: '$10.75', notes: 'Need one more next month'}],
-              total: '$45.78'
+              items: [{category: 'auto', purchase: 'oil change', quantity: 1, price: '$10.75', total: '$10.75', notes: 'Need one more'}],
+              total: this.categoryTotal
             }
             ];
-
-  // budget = [{category: 'grocery', purchase: 'milk', quantity: 1, notes: }];
 
   constructor() {}
   addCategory(event): void {
@@ -55,17 +56,26 @@ export class BudgetPageComponent implements OnInit {
         if (this.inputPurchaseValue !== null && this.inputQuantityValue !== null &&
           this.inputPriceValue !== null && this.inputNotesValue !== null) {
           if (event.keyCode === 13) {
+            this.calcItemTotal();
+            // this.calcCategoryTotal(parentIndex);
             this.budget[parentIndex].items.push({
               category: this.budget[parentIndex].categoryName, purchase: this.inputPurchaseValue,
-              quantity: this.inputQuantityValue, price: this.inputPriceValue, notes: this.inputNotesValue
+              quantity: this.inputQuantityValue, price: this.inputPriceValue, total: this.total, notes: this.inputNotesValue
             });
-            this.inputPurchaseValue = '';
-            this.inputQuantityValue = null;
-            this.inputPriceValue = '';
-            this.inputNotesValue = '';
+            this.resetValue();
           }
         }
       }
+    }
+  }
+  calcCategoryTotal(parentIndex): void {
+    for (let i = 0, l = this.budget[parentIndex].items.length; i < l; i ++) {
+      this.budget[parentIndex].items[i].total = this.budget[parentIndex].items[i].total.slice(1);
+      console.log(this.budget[parentIndex].items[i].total);
+      let myNumber: number = Number(this.budget[parentIndex].items[i].total);
+      console.log(myNumber);
+      this.categoryTotal += myNumber;
+      console.log(this.categoryTotal);
     }
   }
   addItemMouse(parentIndex): void {
@@ -75,20 +85,41 @@ export class BudgetPageComponent implements OnInit {
         this.inputPriceValue !== '' && this.inputNotesValue !== '') {
         if (this.inputPurchaseValue !== null && this.inputQuantityValue !== null &&
           this.inputPriceValue !== null && this.inputNotesValue !== null) {
+          this.calcItemTotal();
           this.budget[parentIndex].items.push({
             category: this.budget[parentIndex].categoryName, purchase: this.inputPurchaseValue,
-            quantity: this.inputQuantityValue, price: this.inputPriceValue, notes: this.inputNotesValue
+            quantity: this.inputQuantityValue, price: this.inputPriceValue, total: this.total, notes: this.inputNotesValue
           });
-          this.inputPurchaseValue = '';
-          this.inputQuantityValue = null;
-          this.inputPriceValue = '';
-          this.inputNotesValue = '';
+          this.resetValue();
         }
       }
     }
   }
   deleteItem(parentIndex, index): void {
     this.budget[parentIndex].items.splice(index, 1);
+  }
+  calcItemTotal(): void {
+    if (!isNaN(this.inputQuantityValue)) {
+      this.inputPriceValue = this.inputPriceValue.slice(1);
+      this.inputPriceValue = Number(this.inputPriceValue);
+      this.total = this.inputPriceValue * this.inputQuantityValue;
+      this.inputPriceValue = Math.round(this.inputPriceValue * 100) / 100;
+      this.inputPriceValue = '$' + this.inputPriceValue;
+      if (this.inputPriceValue.length === 4) {
+        this.inputPriceValue = this.inputPriceValue + '0';
+      }
+      this.total = Math.round(this.total * 100) / 100;
+      this.total = '$' + this.total;
+      if (this.total.length ===  4) {
+        this.total = this.total + '0';
+      }
+    }
+  }
+  resetValue(): void {
+    this.inputPurchaseValue = '';
+    this.inputQuantityValue = null;
+    this.inputPriceValue = '';
+    this.inputNotesValue = '';
   }
 
   ngOnInit() {
