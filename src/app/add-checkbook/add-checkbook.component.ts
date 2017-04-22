102495f8e49ee6cfde7f3981f2a585ddf59be047
joybob22 <braydenlemke8@gmail.com>
2017-04-22 02:48:50 -0600

    added some form validation to add checkbook

--- src/app/add-checkbook/add-checkbook.component.ts
+++ src/app/add-checkbook/add-checkbook.component.ts
@@ -14,6 +14,10 @@ export class AddCheckbookComponent implements OnInit {
   checkbookIndex = 0;
   plusMinus;
   plusMinusInfo = ['-', '+'];
+  checkbookErrorMessage;
+  itemErrorMessage;
+  checkbookError = false;
+  itemError = false;
 
   constructor() { }
 
@@ -25,30 +29,35 @@ export class AddCheckbookComponent implements OnInit {
         {
           date: '01/01/2011',
           money: '300',
+          add: '-',
           description: 'Found Money',
           newAmount: 1700
         },
         {
           date: '01/01/2011',
           money: '450',
+          add: '+',
           description: 'Bought Dog',
           newAmount: 1400
         },
         {
           date: '01/01/2011',
           money: '900',
+          add: '-',
           description: 'Got Paid',
           newAmount: 1850
         },
         {
           date: '01/01/2011',
           money: '50',
+          add: '-',
           description: 'Video Game',
           newAmount: 950
         },
         {
           date: '01/01/2011',
           money: '450',
+          add: '+',
           description: 'Fixed Car',
           newAmount: 1000
         }
@@ -60,42 +69,71 @@ export class AddCheckbookComponent implements OnInit {
   }
 
   addNewCheckbook() {
-    this.checkbooks.push({
-      name: this.checkbookName,
-      items: []
-    });
-    console.log(this.checkbooks);
-    this.checkbookName = "";
+    if(this.checkbookName != '' && this.checkbookName != undefined) {
+      this.checkbooks.push({
+        name: this.checkbookName,
+        items: []
+      });
+      this.checkbookName = "";
+      this.checkbookError = false;
+    } else {
+      this.checkbookErrorMessage = 'Please enter a name for your checkbook';
+      this.checkbookError = true;
+    }
+
   }
 
   addNewItem() {
-    let amount;
-    if(this.checkbooks[this.checkbookIndex].items.length > 0) {
-      let preNum = this.checkbooks[this.checkbookIndex].items[0].newAmount;
-      if(this.plusMinus === '+') {
-        amount = Number(this.money) + preNum;
+    if(this.date != '' && this.date != undefined) {
+      if(this.plusMinus != undefined) {
+        if(this.money != '' && this.money != undefined) {
+          if(this.description != '' && this.description != undefined) {
+            this.itemError = false;
+            let amount;
+            if(this.checkbooks[this.checkbookIndex].items.length > 0) {
+              let preNum = this.checkbooks[this.checkbookIndex].items[0].newAmount;
+              if(this.plusMinus === '+') {
+                amount = Number(this.money) + preNum;
+              } else {
+                amount = preNum - Number(this.money);
+              }
+              console.log(this.plusMinus);
+            } else {
+              if(this.plusMinus === '+') {
+                amount = this.money;
+              } else {
+                amount = 0 - Number(this.money);
+              }
+            }
+            this.checkbooks[this.checkbookIndex].items.unshift({
+              date: this.date,
+              money: this.money,
+              add: this.plusMinus,
+              description: this.description,
+              newAmount: amount
+            });
+            console.log(this.checkbooks);
+            this.date = '';
+            this.money = '';
+            this.description = '';
+          } else {
+            this.itemError = true;
+            this.itemErrorMessage = "Please enter a description";
+          }
+        } else {
+          this.itemError = true;
+          this.itemErrorMessage = "Please enter an amount of money that was deposited/withdrawal"
+        }
       } else {
-        amount = preNum - Number(this.money);
+        this.itemError = true;
+        this.itemErrorMessage = "Please indicate whether money should be subtracted or added"
       }
-      console.log(this.plusMinus);
     } else {
-      if(this.plusMinus === '+') {
-        amount = this.money;
-      } else {
-        amount = 0 - Number(this.money);
-      }
+      this.itemError = true;
+      this.itemErrorMessage = "Please enter a date transaction was made"
     }
 
-    this.checkbooks[this.checkbookIndex].items.unshift({
-      date: this.date,
-      money: this.money,
-      description: this.description,
-      newAmount: amount
-    });
-    console.log(this.checkbooks);
-    this.date = '';
-    this.money = '';
-    this.description = '';
+
   }
 
 
