8aa7a73b29006dfaeb7dcf151f49b4a5b0757fef
Andrew <ward.spacecase@gmail.com>
2017-04-22 03:09:02 -0600

    database uno persona

--- src/app/dashboard-page/dashboard-page.component.ts
+++ src/app/dashboard-page/dashboard-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AngularFire, AngularFireDatabase, FirebaseListObservable} from "angularfire2";
+import {AuthService} from "../angular-fire/auth-service.service";
 
 @Component({
   selector: 'app-dashboard-page',
@@ -10,16 +12,34 @@ export class DashboardPageComponent implements OnInit {
   user: object;
   open: boolean;
 
-  constructor() { }
+  key: any;
+
+  users: FirebaseListObservable<any>;
+  firebaseUser: any;
+
+  constructor(public af: AngularFire, private _auth: AuthService ) {
+
+    this.firebaseUser = af.database.list("/users");
+    this.firebaseUser.subscribe(data => {
+      this.users = data[1];
+      console.log(this.users);
+
+    });
+
+
+
+
+
+  }
 
   ngOnInit() {
 
+    this.user = {
+      time: 'Good Morning',
+      name: ''
+    };
     this.open = false;
 
-    this.user = {
-      name: "Bob",
-      time: "Good afternoon"
-    }
 
   }
 
