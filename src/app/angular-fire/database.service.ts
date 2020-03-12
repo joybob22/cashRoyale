ab7915009751e05d37d815139f47339756524275
Andrew Ward <ward.andrew235@gmail.com>
2020-03-12 09:23:10 -0600

    removed old versions of Highcharts & Angular Fire, added updated Angular Fire

--- src/app/angular-fire/database.service.ts
+++ src/app/angular-fire/database.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@angular/core';
 // import 'rxjs/add/operator/map';
-import {AngularFireModule} from "angularfire2";
-
 
 @Injectable()
 export class DatabaseService {
-
-
+    constructor() {
+    }
+    
 }
