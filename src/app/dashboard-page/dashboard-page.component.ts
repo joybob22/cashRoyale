786d4bf9faf0787c385e57fcfc331ca75e1d8d48
Andrew Ward <ward.andrew235@gmail.com>
2020-03-18 16:21:09 -0600

    Successfully pulling from Firebase

--- src/app/dashboard-page/dashboard-page.component.ts
+++ src/app/dashboard-page/dashboard-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {AuthService} from "../angular-fire/auth-service.service";
 import {Router} from "@angular/router";
 import {User} from "../user.model";
+import { DatabaseService } from '../angular-fire/database.service';
 
 @Component({
   selector: 'app-dashboard-page',
@@ -13,16 +14,16 @@ export class DashboardPageComponent implements OnInit {
   user: User;
   open: boolean;
   userData: any;
+  users: any;
 
-
-  constructor( private _data: AuthService, private router: Router) {
+  constructor( private _data: AuthService, private router: Router, private _db: DatabaseService) {
       this.userData = _data.userData;
   }
 
   ngOnInit() {
 
+    this._db.usersObservable.subscribe(d => console.log(d), err => console.log(err), () => console.log("Completed."));
     if(!this._data.userExists){
-      console.log("oooof");
       this.router.navigate(['../homePage']);
     }
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyD2ySAfpDimxQ1cIMlbmajh8nGN1vyAzV0',
+  authDomain: 'cash-royale.firebaseapp.com',
+  databaseURL: 'https://cash-royale.firebaseio.com',
+  projectId: 'cash-royale',
+  storageBucket: 'cash-royale.appspot.com',
+  messagingSenderId: '977860527900'
+  }
 };
